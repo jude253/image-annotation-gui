1595116d2896793a49d4b632ea09502467160ded
Jude <judecapachietti@gmail.com>
2024-08-05 20:43:05 -0700

    add initial transform_to_coco_format.py script

--- src/image_annotation_gui/utils.py
+++ src/image_annotation_gui/utils.py
@@ -1,7 +1,7 @@
 import json
 import os
+import shutil
 from typing import List, Optional
-
 from pydantic import BaseModel
 
 
@@ -48,6 +48,103 @@ def get_list_of_image_paths():
     return image_paths
 
 
+def get_coco_data_root_dir(data_parent_folder=''):
+    coco_root = find_dir_path('coco_format_data')
+    data_dir = os.path.join(coco_root, data_parent_folder, 'data')
+
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    return data_dir
+        
+
+class Category(BaseModel):
+    id: int
+    name: str
+
+class Image(BaseModel):
+    id: int
+    file_name: str  # jpeg
+
+class BoundingBox(BaseModel):
+    x: float  # top left
+    y: float  # top left
+    w: float
+    h: float
+
+    def to_list(self):
+        return [self.x, self.y, self.w, self.h]
+
+class Annotation(BaseModel):
+    id: int
+    image_id: int
+    category_id: int
+    bbox: list[float]
+
+class LabelsJson(BaseModel):
+    """
+    This class only has what is needed for the media pipes training:
+    https://github.com/google-ai-edge/mediapipe-samples/blob/main/examples/customization/object_detector.ipynb
+    
+    
+    https://cocodataset.org/#format-data
+    """
+    categories: list[Category]
+    images: list[Image]
+    annotations: list[Annotation]
+
+    def get_image_id(self, file_name):
+        image_id = None
+        for image in self.images:
+            if file_name == image.file_name:
+                image_id = image.id
+                break
+        
+        if image_id is None:
+            new_image = Image(
+                id=len(self.images),
+                file_name=file_name
+            )
+            self.images.append(new_image)
+            image_id = new_image.id
+
+        return image_id
+    
+    def get_category_id(self, category_name):
+        category_id = None
+        for category in self.categories:
+            if category_name == category.name:
+                category_id = category.id
+                break
+        
+        if category_id is None:
+            new_category = Category(
+                id=len(self.categories),
+                name=category_name
+            )
+            self.categories.append(new_category)
+            category_id = new_category.id
+
+        return category_id
+    
+    def add_annotation(self, annotation):
+        self.annotations.append(annotation)
+
+    def to_json(self):
+        return json.dumps({
+            "categories": [c.__dict__ for c in self.categories],
+            "images": [i.__dict__ for i in self.images],
+            "annotations": [a.__dict__ for a in self.annotations],
+        })
+    
+    def write_file(self, data_parent_folder=''):
+        data_dir = get_coco_data_root_dir(data_parent_folder=data_parent_folder)
+        label_json_file_path = os.path.join(data_dir, 'label.json')
+
+        with open(label_json_file_path, 'w') as file:
+            file.write(self.to_json())
+
+
 class ImageAnnotation(BaseModel):
     image_full_path: str
     image_file_name: str
@@ -69,12 +166,11 @@ class ImageAnnotation(BaseModel):
         self.layout_shapes_data = new_layout_shapes_data
 
 
-    
-
 class ImageAnnotationBuilder(BaseModel):
     image_annotations: List[ImageAnnotation] | None = None
     image_names: list[str] | None = None
     current_image_annotation: ImageAnnotation | None = None
+    label_json: LabelsJson | None = None
     
     def setupImageAnnoations(self, image_paths):
         self.image_annotations = []
@@ -102,7 +198,57 @@ class ImageAnnotationBuilder(BaseModel):
                 return image_annotation
         
         return None
+    
+    def set_label_json(self):
+        label_json = LabelsJson(
+            categories=[],
+            images=[],
+            annotations=[]
+        )
+        for image_annotation in self.image_annotations:
+            layout_shapes_data = image_annotation.layout_shapes_data
+            for layout_shape in layout_shapes_data:
+                x0 = layout_shape['x0']
+                y0 = layout_shape['y0']
+                x1 = layout_shape['x1']
+                y1 = layout_shape['y1']
+                x_top_left = min(x0, x1)
+                y_top_left = min(y0, y1)
+                w = abs(x1 - x0)
+                h = abs(y1 - y0)
+                bbox = BoundingBox(
+                    x=x_top_left,
+                    y=y_top_left,
+                    w=w,
+                    h=h
+                )
+                category_name = layout_shape['label']['text']
+                category_id = label_json.get_category_id(category_name)
+                image_id = label_json.get_image_id(image_annotation.image_file_name)
+                annotation = Annotation(
+                    id=len(label_json.annotations),
+                    image_id=image_id,
+                    category_id=category_id,
+                    bbox=bbox.to_list()
+                )
+                label_json.add_annotation(annotation)
+        self.label_json = label_json
 
+    def create_coco_format_data_archive(self, data_parent_folder=''):
+        """
+        Create a zip file of the coco format data
+        """
+        data_dir = get_coco_data_root_dir(data_parent_folder=data_parent_folder)
+        self.set_label_json()
+        self.label_json.write_file(data_parent_folder=data_parent_folder)
+
+        for image_annotation in self.image_annotations:
+            old_image_file_path = image_annotation.image_full_path
+            new_image_file_path = os.path.join(data_dir, image_annotation.image_file_name)
+            shutil.copyfile(old_image_file_path, new_image_file_path)
+
+        shutil.make_archive(data_dir, 'zip', data_dir)
+        shutil.rmtree(data_dir)
 
 
 def create_image_annotation_builder():
@@ -116,5 +262,5 @@ if __name__ == '__main__':
 
     image_annotation_builder = create_image_annotation_builder()
     
-    for imageAnnotation in image_annotation_builder.image_annotations:
-        print(imageAnnotation)
+    for image_annotation in image_annotation_builder.image_annotations:
+        print(image_annotation)
